5716f528123a5998edaf9c1a90d486dbec44baaa
kampos <kampos@gmail.com>
2025-03-08 17:24:54 +0100

    prouyecto final compilado y listo

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,16 +1,28 @@
-/**
- * Import dependencies from node_modules
- * see commented examples below
- */
+import AOS from 'aos';
 
-// import 'some-node-module';
-// import SomeModule from 'some-node-module';
 
-/**
- * Write any other JavaScript below
- */
+document.addEventListener('DOMContentLoaded', function() {
+  AOS.init({
+    duration: 1000,  
+    once: true,      
+   
+  });
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+  const sliderWrapper = document.querySelector('.gallery-slider__wrapper');
+  const slides = document.querySelectorAll('.gallery-slider__slide');
+  let currentIndex = 0;
+  const totalSlides = slides.length;
+
+  if (sliderWrapper && slides.length > 0) {
+    function showSlide(index) {
+      currentIndex = (index + totalSlides) % totalSlides;
+      sliderWrapper.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+
+    setInterval(() => {
+      showSlide(currentIndex + 1);
+    }, 3000);
+
+    showSlide(currentIndex);
+  }
+});
